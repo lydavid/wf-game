7a7c029dcebfa1b812abf30ea011c3f25777f0a7
Felipe Vicencio-Heap <felipe.estevan@gmail.com>
2018-01-29 22:41:40 -0500

    added some scripts that need to be modified

---
+++ Warp Fighters/Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	public float tracking =1;
+	private float rot_x;
+	private float rot_y;
+	private float rot_z;
+	private float distance;
+	private float yaw, pitch, roll;
+
+	// Use this for initialization
+	void Start () {
+		distance = Vector3.Distance(transform.position, player.transform.position);
+	}
+
+	void Update() {
+
+	}
+	
+	// Update is called once per frame
+
+	void LateUpdate () {
+		transform.position = player.transform.position + ((-distance)*transform.forward) ; //update position
+		cameraRotate ();
+
+	}
+	void cameraRotate (){
+		if (Input.GetKey ("f")) {
+			rot_z = 0;
+			rot_x = Input.GetAxis ("Mouse X");
+			rot_y = Input.GetAxis ("Mouse Y");
+			rot_z = Input.GetMouseButton (0) ? 1 : 0;
+			rot_z = rot_z - (Input.GetMouseButton (1) ? 1 : 0);
+			transform.RotateAround (player.transform.position, transform.up, (8 / 3) * rot_x * tracking);
+			transform.RotateAround (player.transform.position, transform.right, (8 / 3) * rot_y * tracking);
+			transform.RotateAround (player.transform.position, transform.forward, (8 / 3) * rot_z * tracking);
+
+			distance = distance + Input.mouseScrollDelta.y;
+		}
+	}
+	void cameraFollow() {
+
+			yaw = Input.GetAxis ("Horizontal");
+			pitch = Input.GetAxis ("Vertical");
+			roll = Input.GetKey ("q")?1:0;
+			roll = roll - (Input.GetKey ("e")?1:0);
+
+			transform.RotateAround (player.transform.position, transform.up, (2/3) * yaw);
+			transform.RotateAround (player.transform.position, transform.right, (2/3) * pitch);
+			transform.RotateAround (player.transform.position, transform.forward, (2/3) * roll);
+	}
+}
+
---
+++ Warp Fighters/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private float yaw;
+	private float pitch;
+	private float roll;
+	private int flappiness;
+	private bool flappable = true;
+
+	public int flapcap = 100;
+	public float gravity = 20;
+	public float flapower = 10;
+	public float steer = 2;
+	public float speed = 1;
+	public GameObject player;
+	public Rigidbody p_physics;
+	public float lifty = 20;
+
+
+	// Use this for initialization
+	void Start () {
+		player.transform.forward = Vector3.forward;
+		flappiness = flapcap;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		jets();
+		//grav();
+		//glide ();
+		//lift ();
+		//flap();
+		Debug.Log (flappiness.ToString());
+	}
+
+	void Update (){
+		steering ();
+	}
+	void steering() { // Maintaining forward momentum TODO
+		yaw = Input.GetAxis ("Horizontal");
+		pitch = Input.GetAxis ("Vertical");
+		roll = Input.GetKey ("q")?1:0;
+		roll = roll - (Input.GetKey ("e")?1:0);
+
+		player.transform.RotateAround (player.transform.position, player.transform.up, yaw * steer);
+		player.transform.RotateAround (player.transform.position, player.transform.right, pitch * steer);
+		player.transform.RotateAround (player.transform.position, player.transform.forward, roll * steer);
+	}
+
+	void glide() {
+		if (Vector3.Dot(p_physics.velocity, -player.transform.up) != 0) { //Change to player down velocity TODO
+			p_physics.AddForce (player.transform.forward * speed * (-p_physics.velocity.y));
+		}
+	}
+
+	void jets () {
+		p_physics.velocity = player.transform.forward*speed ;
+	}
+
+	void lift () { // if the player is moving forward add an up force
+		if (Vector3.Dot(p_physics.velocity, player.transform.forward) > 0) {
+			p_physics.AddForce (player.transform.up * lifty);
+		}
+	}
+
+	void flap () {
+		if (flappable == true) {
+			if (Input.GetKey ("p")) {
+				if (flappiness > 0) {
+					p_physics.AddForce (flapower*player.transform.forward + flapower*player.transform.up);
+					flappiness = flappiness -1;
+				} else {
+					flappable = false;
+				}
+			} else {
+				if (flappiness < flapcap) {
+					flappiness = flappiness + 1;
+				}
+			}
+		} else {
+			if (flappiness < flapcap) {
+				flappiness = flappiness + 1;
+			} else {
+				flappable = true;
+			}
+		}
+	}
+
+	void grav () {
+		p_physics.AddForce (Vector3.down*gravity);
+	}
+}
+
