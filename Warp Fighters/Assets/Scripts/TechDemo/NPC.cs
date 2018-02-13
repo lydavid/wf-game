de41e0d0da93fdbdb59e566a6321f1802d6cad8a
xyecs <runedesired2@gmail.com>
2018-02-12 22:15:52 -0500

    Quick Sample Level

---
+++ Warp Fighters/Assets/Scripts/TechDemo/NPC.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(player.transform);
+	}
+
+}
---
+++ Warp Fighters/Assets/Scripts/TechDemo/Trigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+	private bool playerNotified = false;
+	[SerializeField]
+	private GameObject NPC;
+	private AudioSource instructions;
+	// Use this for initialization
+	void Start () {
+		instructions = NPC.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (!playerNotified){
+			instructions.Play();
+		}
+		playerNotified = true;
+		if (c.CompareTag("Player")){
+			Debug.Log("player entered");
+		}
+	}
+
+	/* 
+	void onCollisionEnter(Collision c){
+		Debug.Log("collision");
+		if (c.gameObject.tag == "NPCTrigger"){
+			Debug.Log("player entered");
+		}
+	}*/
+}
