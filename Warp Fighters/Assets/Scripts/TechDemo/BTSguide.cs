5aa98d10ac41443dc618ab3ddd03bfc3f2bcd32c
xyecs <runedesired2@gmail.com>
2018-02-22 16:08:38 -0500

    BTS version guide always follows mouse

--- Warp Fighters/Assets/Scripts/TechDemo/BTSguide.cs
+++ Warp Fighters/Assets/Scripts/TechDemo/BTSguide.cs
@@ -5,44 +5,54 @@ using UnityEngine;
 public class BTSguide : MonoBehaviour {
 
 	[SerializeField]
-	private Camera camera;
-	private GameObject warpGuidePrefab;
-	[SerializeField]
-	private Material standardWarpGuide;
+	private Camera cam; // BTS cam attached to player
+	public GameObject warpGuide;
 
-	// Use this for initialization
-	void Start () {
-		warpGuidePrefab = (GameObject)Resources.Load("Prefabs/Warp Guide", typeof(GameObject));
-	}
+	private bool warpToggle = false;
+
+	// distance of warpguide from player/camera, should be min 4 or 5
+	private float camDist = 4f;
+	private const float minCamDist = 4f;
+	private const float maxCamDist = 100f;
 	
-	// Update is called once per frame
+
+	void Start () {
+	
+	}
+
+
 	void Update () {
+		if (Input.GetButtonDown("Fire2")){
+			if (warpToggle){
+				warpToggle = false;
+			}else{
+				warpToggle = true;
+			}
+		}
+		float mw = Input.GetAxis("Mouse ScrollWheel");
+		WarpGuideAnimate(mw);
+        //cam.nearClipPlane
+	}
 
-		if (Input.GetButtonDown("Fire2"))
-        {
+	void WarpGuideAnimate(float mouseWheel){
 
-			Vector3 mousePoint = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
+		// Making sure dist between warp guide and player always looks ok
+		if (camDist <= minCamDist){
+			camDist = minCamDist;
+		}
+		if (camDist >= maxCamDist){
+			camDist = maxCamDist;
+		}
+
+		// If warp guide on
+		if (warpToggle){
+			camDist += mouseWheel;
+			Vector3 mousePoint = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDist));
 			Debug.Log(mousePoint);
-            GameObject warpGuide;
-
-            warpGuide = Instantiate(warpGuidePrefab, mousePoint, transform.rotation);
-            foreach (Renderer renderer in warpGuide.GetComponentsInChildren<Renderer>())
-            {
-
-				renderer.material = standardWarpGuide;
-                //warpGuide.GetComponent<WarpGuideController>().SetAsSpeedWarpGuide(false);
-                /*if (isSpeedWarp)
-                {
-                    renderer.material = altWarpGuide;
-                    warpGuide.GetComponent<WarpGuideController>().SetAsSpeedWarpGuide(true);
-                } else
-                {
-                    renderer.material = standardWarpGuide;
-                    warpGuide.GetComponent<WarpGuideController>().SetAsSpeedWarpGuide(false);
-                }*/
-                
-                    
-            }    
-        }
+			warpGuide.SetActive(true);
+			warpGuide.transform.position = mousePoint;
+		}else{
+			warpGuide.SetActive(false);
+		}
 	}
 }
