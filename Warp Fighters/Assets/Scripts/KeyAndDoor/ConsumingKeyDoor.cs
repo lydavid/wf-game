bdd156215dc242565614690110e41ad0485ccf2e
David Ly <d.ly@mail.utoronto.ca>
2018-02-27 20:29:52 -0500

    Can actually go through transparent walls now. Also commented out consuming player's key -> keys are no longer one use

--- Warp Fighters/Assets/Scripts/KeyAndDoor/ConsumingKeyDoor.cs
+++ Warp Fighters/Assets/Scripts/KeyAndDoor/ConsumingKeyDoor.cs
@@ -10,25 +10,33 @@ public class ConsumingKeyDoor : MonoBehaviour {
 
     GameObject player;  // These types of doors will need to keep a reference to the player at start
     Material originalMaterial;
+    bool keyFound; // flag to prevent unnecessary find statements after player has found key
 
 
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         originalMaterial = gameObject.GetComponent<Renderer>().material;
+        keyFound = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Turn object transparent to indicate player can warp through
-		if (player.GetComponent<CollectedKeysManager>().HasKey(doorType))
+        if (!keyFound)
         {
-            gameObject.GetComponent<Renderer>().material = transparentMaterial;
-        } else
-        {
-            // otherwise change object material back to original
-            gameObject.GetComponent<Renderer>().material = originalMaterial;
+            // Turn object transparent to indicate player can warp through
+            if (player.GetComponent<CollectedKeysManager>().HasKey(doorType))
+            {
+                gameObject.GetComponent<Renderer>().material = transparentMaterial;
+                Destroy(gameObject.GetComponent<MeshCollider>());
+                keyFound = true;
+            }
+            /*else // currently we aren't implementing a way to lock out the door again, mostly for the convenience of the player
+            {
+                // otherwise change object material back to original
+                //gameObject.GetComponent<Renderer>().material = originalMaterial;
+            }*/
         }
 	}
 
