d56b3c18b93a72b6d68844846bd52d4a2bdf5340
xyecs <runedesired2@gmail.com>
2018-02-28 21:14:03 -0500

    audio controls

---
+++ Warp Fighters/Assets/Scripts/AudioController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	AudioSource AddAudio (GameObject obj, AudioClip clip, bool loop, bool playOnAwake, float volume) {
+		AudioSource audio = obj.AddComponent<AudioSource>();
+		audio.clip = clip;
+		audio.loop = loop;
+		audio.playOnAwake = playOnAwake;
+		audio.volume = volume;
+		return audio;
+	}
+
+}
