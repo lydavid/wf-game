1b50d3d08e8b359ac1fd6b8f5395a2cbc0d33acc
xyecs <runedesired2@gmail.com>
2018-03-01 00:12:01 -0500

    draw hearts

---
+++ Warp Fighters/Assets/Scripts/DrawHearts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawHearts : MonoBehaviour {
+
+	public Texture heart;
+	private int init_hearts;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		init_hearts = GetComponent<HPManager>().healthPoints;
+	}
+
+	void OnGUI () {
+		for (int i = 0; i < init_hearts; i++) {
+			GUI.DrawTexture(new Rect(i*50, 0, 50, 50), heart);
+		}
+	}
+}
