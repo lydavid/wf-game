c06cc4a5db640dbda0e9fe9f3e6b6dda3a706e92
xyecs <runedesired2@gmail.com>
2018-02-28 23:16:46 -0500

    restart

---
+++ Warp Fighters/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+
+	public Button restartBtn;
+
+	// Use this for initialization
+	void Start () {
+        restartBtn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Alpha");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
