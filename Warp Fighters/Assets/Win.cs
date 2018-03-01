64d4a0d83933f7332bba0739d5501580db3b78ca
David Ly <d.ly@mail.utoronto.ca>
2018-03-01 01:34:20 -0500

    Winnable game

---
+++ Warp Fighters/Assets/Win.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Win");
+        } 
+    }
+}
