69375afc582c24211d968c4e396452798f9da4ef
David Ly <d.ly@mail.utoronto.ca>
2018-03-01 03:58:42 -0500

    Minor fixes: restart game with menu button

---
+++ Warp Fighters/Assets/DisableInstructions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInstructions : MonoBehaviour {
+
+    GameObject inst;
+
+    bool isEnabled;
+
+	// Use this for initialization
+	void Start () {
+        inst = GameObject.FindGameObjectsWithTag("Instructions")[0];
+        isEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Window Button"))
+        {
+            if (isEnabled)
+            {
+                inst.SetActive(false);
+                isEnabled = false;
+            } else
+            {
+                inst.SetActive(true);
+                isEnabled = true;
+            }
+        }
+	}
+}
