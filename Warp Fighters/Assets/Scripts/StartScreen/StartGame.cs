879408e68a2658e1d7a0316082e173fd211aa150
xyecs <runedesired2@gmail.com>
2018-03-07 18:13:11 -0500

    Game Start Screen

---
+++ Warp Fighters/Assets/Scripts/StartScreen/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Menu Button"))
+        {
+			audio.Play();
+            SceneManager.LoadScene("Alpha");
+
+        }
+	}
+}
