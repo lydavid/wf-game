b5378f8db770575bb1651942e4b418ada35f5c15
David Ly <d.ly@mail.utoronto.ca>
2018-03-07 22:37:25 -0500

    Falling off will respawn player at last position they were standing.

---
+++ Warp Fighters/Assets/RespawnOnGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// QoL Script
+// This script tracks the last position the player was standing on classified as "Ground"
+// and when the player falls off, respawn them there.
+public class RespawnOnGround : MonoBehaviour {
+
+    Vector3 lastGroundPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            lastGroundPosition = transform.position;
+            Debug.Log(lastGroundPosition);
+        } else if (other.gameObject.tag == "BottomPlane")
+        {
+            // Respawn player at last ground position
+            transform.position = lastGroundPosition;
+            // Remove any velocity in case player had warped off from there
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
