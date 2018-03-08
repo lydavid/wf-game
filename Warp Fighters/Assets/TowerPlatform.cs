b5d76f3a71da3337acab9a84bd6bdce28b2747af
xyecs <runedesired2@gmail.com>
2018-03-08 10:34:57 -0500

    tower platform moves up

---
+++ Warp Fighters/Assets/TowerPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlatform : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.tag == "Player") {
+			anim.SetBool("PlatformStepped", true);
+		}
+	}
+}
