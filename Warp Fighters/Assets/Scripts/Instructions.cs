3829651c21f75be50229c6e5a6c3c91ec9a0874f
xyecs <runedesired2@gmail.com>
2018-03-13 19:11:00 -0400

    instructions 3 lines at begin

--- Warp Fighters/Assets/Scripts/Instructions.cs
+++ Warp Fighters/Assets/Scripts/Instructions.cs
@@ -10,6 +10,8 @@ public class Instructions : MonoBehaviour {
 
 	public GameObject player;
 
+	private bool shown = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,14 +19,31 @@ public class Instructions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (shown == false)
+		{
+			StartCoroutine(showInstructions());
+			shown = true;
+		}
 		
 	}
 
 	void OnTriggerEnter (Collider hit) 
 	{
-		Debug.Log("ting");
-	
+		// move player under the terrain
 		player.transform.Translate(new Vector3(0,-1,0));
-		
+	}
+
+	IEnumerator showInstructions ()
+	{
+		line1.SetActive(true);
+		yield return new WaitForSeconds(3f);
+		line1.SetActive(false);
+		line2.SetActive(true);
+		yield return new WaitForSeconds(3f);
+		line2.SetActive(false);
+		line3.SetActive(true);
+		yield return new WaitForSeconds(3f);
+		line3.SetActive(false);
+
 	}
 }
