ebe08caf19bb8faada03d4f4afdcd148e59c1c20
xyecs <runedesired2@gmail.com>
2018-03-18 14:46:21 -0400

    TPS camera tests

---
+++ Warp Fighters/Assets/Scripts/CameraTest/CameraTPS.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTPS : MonoBehaviour {
+
+
+	public GameObject player;
+	public Transform camTransform;
+	public float distance = 6.0f;
+	public float sensitivity = 200.0f;
+	private float h, v = 0.0f;
+	private bool clipped = false;
+
+	private void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+		camTransform = transform;
+    }
+
+    private void Update()
+    {
+		h += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        v += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+		h += Input.GetAxis("Right Stick X") * sensitivity * Time.deltaTime;
+        v += Input.GetAxis("Right Stick Y") * sensitivity * Time.deltaTime;
+
+		v = Mathf.Clamp(v, -70f, 70f);
+		cameraRay();
+    }
+
+    private void LateUpdate()
+    {
+		Vector3 dir = new Vector3(0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler(v, h, 0);
+		camTransform.position = player.transform.position + rotation * dir;
+		camTransform.LookAt(player.transform.position);
+    }
+
+	private void cameraRay ()
+	{
+		
+		RaycastHit hit;
+		Vector3 start = transform.position;
+		Vector3 dir = transform.forward;
+
+		Debug.DrawRay(start, dir, Color.green);
+
+		if (Physics.Raycast(start, dir, out hit))
+		{
+			if (hit.transform.gameObject.tag == "Player")
+			{
+				clipped = false;
+			} 
+			else 
+			{
+				clipped = true;
+			}
+		}
+
+		Debug.Log(clipped);
+	}
+}
---
+++ Warp Fighters/Assets/Scripts/CameraTest/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+
+       	transform.Translate(mH * speed * Time.deltaTime, 0f, mV * speed * Time.deltaTime);
+	}
+}
