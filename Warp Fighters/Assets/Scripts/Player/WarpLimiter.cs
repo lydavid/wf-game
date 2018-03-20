1364ecdfbbd1183bca43bd1fffa153c2ca09d196
David Ly <d.ly@mail.utoronto.ca>
2018-03-20 16:04:25 -0400

    UI: Warp charges display update to look nicer. Not final.

--- Warp Fighters/Assets/Scripts/Player/WarpLimiter.cs
+++ Warp Fighters/Assets/Scripts/Player/WarpLimiter.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarpLimiter : MonoBehaviour {
 
-    public Texture warpChargeBarSide;
-    public Texture warpChargeBarMiddle;
+    //public Texture warpChargeBarSide;
+    //public Texture warpChargeBarMiddle;
 
 
     public bool canWarp;  // other scripts in player game object must confirm with this that they are able to warp before doing so
@@ -14,14 +15,22 @@ public class WarpLimiter : MonoBehaviour {
     public float warpRechargeTime;
     public float warpRechargeTimeProgress;
 
+    Text NumWarpChargesText;
+    Text WarpChargesDisplayText;
+    RectTransform WarpChargesDisplayBackdrop;
+
     // Use this for initialization
     void Start() {
         canWarp = true;
         maxWarpCharges = 5;
         warpCharges = maxWarpCharges;
-        warpRechargeTime = 1.5f;
+        warpRechargeTime = 0.5f;
         warpRechargeTimeProgress = warpRechargeTime;
 
+        NumWarpChargesText = GameObject.Find("NumWarpChargesText").GetComponent<Text>();
+        WarpChargesDisplayText = GameObject.Find("WarpChargesDisplayText").GetComponent<Text>();
+        WarpChargesDisplayBackdrop = GameObject.Find("WarpChargesDisplayBackdrop").GetComponent<RectTransform>();
+        UpdateUI();
     }
 
     // Update is called once per frame
@@ -42,8 +51,34 @@ public class WarpLimiter : MonoBehaviour {
         {
             canWarp = true;
         }
+
+        
 		
 	}
+
+    // Try to not do this every frame, only call this when we know it will change
+    void UpdateUI()
+    {
+
+        // NumWarpChargesText
+        string zeroInFront = "";
+        if (warpCharges < 10)
+        {
+            zeroInFront = "0";
+        }
+        NumWarpChargesText.text = zeroInFront + warpCharges.ToString();
+
+        // WarpChargesDisplayText
+        WarpChargesDisplayText.text = "";
+        for (int i = 0; i < warpCharges; i++)
+        {
+            WarpChargesDisplayText.text += "|";
+        }
+
+        // WarpChargesDisplayTextBackdrop
+        // size varies on maxWarpCharges
+        WarpChargesDisplayBackdrop.sizeDelta = new Vector2(maxWarpCharges * 4, 20);
+    }
 
 
     void Recharge()
@@ -53,6 +88,7 @@ public class WarpLimiter : MonoBehaviour {
         {
             warpCharges += 1;
             warpRechargeTimeProgress = warpRechargeTime;
+            UpdateUI();
         }
     }
 
@@ -60,6 +96,7 @@ public class WarpLimiter : MonoBehaviour {
     public void ConsumeCharge()
     {
         warpCharges -= 1;
+        UpdateUI();
     }
 
 
@@ -68,32 +105,33 @@ public class WarpLimiter : MonoBehaviour {
     {
         maxWarpCharges += charge;
         warpCharges = maxWarpCharges;
+        UpdateUI();
     }
 
 
-    void OnGUI()
-    {
-        for (int i = 0; i < warpCharges; i++)
-        {
-            int barSize = 10;
-            /*if (i > warpCharges)
-            {
-                barSize = Mathf.RoundToInt(Mathf.Floor(warpRechargeTime - warpRechargeTimeProgress) * 10 / warpRechargeTime);
-            } else
-            {
-                barSize = 10;
-            }*/
-            for (int j = 0; j < barSize; j++)
-            {
-                if (j == 0 || j == barSize - 1)
-                {
-                    GUI.DrawTexture(new Rect(i * 10 * 5 + j * 5, 50, 5, 15), warpChargeBarSide);
-                }
-                else
-                {
-                    GUI.DrawTexture(new Rect(i * 10 * 5 + j * 5, 50, 5, 15), warpChargeBarMiddle);
-                }
-            }
-        }
-    }
+    //void OnGUI()
+    //{
+    //    for (int i = 0; i < warpCharges; i++)
+    //    {
+    //        int barSize = 10;
+    //        /*if (i > warpCharges)
+    //        {
+    //            barSize = Mathf.RoundToInt(Mathf.Floor(warpRechargeTime - warpRechargeTimeProgress) * 10 / warpRechargeTime);
+    //        } else
+    //        {
+    //            barSize = 10;
+    //        }*/
+    //        for (int j = 0; j < barSize; j++)
+    //        {
+    //            if (j == 0 || j == barSize - 1)
+    //            {
+    //                GUI.DrawTexture(new Rect(i * 10 * 5 + j * 5, 50, 5, 15), warpChargeBarSide);
+    //            }
+    //            else
+    //            {
+    //                GUI.DrawTexture(new Rect(i * 10 * 5 + j * 5, 50, 5, 15), warpChargeBarMiddle);
+    //            }
+    //        }
+    //    }
+    //}
 }
