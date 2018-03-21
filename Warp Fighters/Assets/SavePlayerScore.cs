efe4f3561bacca74c1a214905354844baf903843
David Ly <d.ly@mail.utoronto.ca>
2018-03-21 17:50:17 -0400

    SaveScore testing. No longer needed probably. Just reference.

---
+++ Warp Fighters/Assets/SavePlayerScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+// This script handles writing all relevant user data to a csv file
+// which will be used for the leaderboards
+public class SavePlayerScore : MonoBehaviour {
+
+    public GameObject b;
+    Text c;
+    int ID;
+
+	// Use this for initialization
+	void Start () {
+        c = b.GetComponent<Text>();
+        if (PlayerPrefs.HasKey(Constants.ID_KEY))
+        {
+            ID = PlayerPrefs.GetInt(Constants.ID_KEY) + 1;
+        } else
+        {
+            ID = 0;
+        }
+        PlayerPrefs.SetInt(Constants.ID_KEY, ID);
+        PlayerPrefs.Save();
+
+        c.text = ID.ToString();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Warp Fighters/Assets/Scripts/Constants.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Constants
+{
+    // Primary key
+    public const string ID_KEY = "ID"; // Unique integer key, order of entry into file
+
+    public const string DATE_KEY = "date"; // The datetime this entry was recorded, stored as string
+
+    // The 3 main columns display on the leaderboards are Rank, Score, Name
+    // Rank will not be stored, as it will continuously be recalculated at the end of a game session
+    public const string SCORE_KEY = "score"; // Player's time in seconds, stored as float
+    public const string NAME_KEY = "name"; // 3 letter chosen name like in old school arcades
+
+    // More info columns
+    public const string KILLS_KEY = "kills"; // num enemies killed
+    public const string WARPS_KEY = "warps"; // num times warped
+
+
+}
