9ade8627fc429bc6d909a3254c1526f103f21e73
David Ly <d.ly@mail.utoronto.ca>
2018-03-23 01:15:37 -0400

    Automatically assign convex to meshcolliders, frictionless material to colliders of obj tagged as "Wall".

---
+++ Warp Fighters/Assets/Automation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Automation : MonoBehaviour {
+
+    public PhysicMaterial frictionlessMaterial;
+
+	// Use this for initialization
+	void Start () {
+
+        /* Automatically make all mesh colliders in scene convex
+         * all obj tagged as Wall given frictionless material
+         */
+        MeshCollider[] meshColliders = GameObject.FindObjectsOfType(typeof(MeshCollider)) as MeshCollider[];
+        foreach (MeshCollider mc in meshColliders)
+        {
+            Debug.Log(mc.gameObject.name);
+            mc.convex = true;
+            if (mc.gameObject.tag == "Wall")
+            {
+                mc.material = frictionlessMaterial;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
