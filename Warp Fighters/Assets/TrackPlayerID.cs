d7b9b81c389e1b114a24619f6d02a056aa09f4e0
David Ly <d.ly@mail.utoronto.ca>
2018-03-29 01:43:03 -0400

    Reset playerprefs on game start. Track player ID on game start.

---
+++ Warp Fighters/Assets/TrackPlayerID.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script keeps track of the ID of the current player (ie i_th player)
+public class TrackPlayerID : MonoBehaviour {
+
+    public int ID;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.HasKey(Constants.ID_KEY))
+        {
+            ID = PlayerPrefs.GetInt(Constants.ID_KEY) + 1;
+        }
+        else
+        {
+            ID = 0;
+        }
+        PlayerPrefs.SetInt(Constants.ID_KEY, ID);
+        PlayerPrefs.Save();
+    }
+}
