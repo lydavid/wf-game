12424101e29ff216810b2dae83bd8a5de090204b
David Ly <d.ly@mail.utoronto.ca>
2018-03-29 02:11:26 -0400

    Game Over screen can be restarted with controller. Deleted unused CSV asset folder.

--- Warp Fighters/Assets/Scripts/GameControl/RestartGame.cs
+++ Warp Fighters/Assets/Scripts/GameControl/RestartGame.cs
@@ -15,7 +15,7 @@ public class RestartGame : MonoBehaviour {
 
     void TaskOnClick()
     {
-        SceneManager.LoadScene("JonLevel");
+        SceneManager.LoadScene("Alpha3.0");
 	}
 	
 	// Update is called once per frame
