6e0a61d9461f69856c1cd882296248944256a67a
David Ly <d.ly@mail.utoronto.ca>
2018-04-03 19:58:58 -0400

    Crystal key halo lighting.

---
+++ Warp Fighters/Assets/KeyLighting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLighting : MonoBehaviour {
+
+    public GameObject cLight;
+    public GameObject yLight;
+    public GameObject mLight;
+    public GameObject bLight;
+
+    KeyType keyType;
+
+    // Use this for initialization
+    void Start () {
+        keyType = GetComponent<CollectibleKey>().keyType;
+        switch (keyType)
+        {
+            case KeyType.Blue:
+                bLight.SetActive(true);
+                break;
+            case KeyType.Cyan:
+                cLight.SetActive(true);
+                break;
+            case KeyType.Magenta:
+                mLight.SetActive(true);
+                break;
+            case KeyType.Yellow:
+                yLight.SetActive(true);
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
